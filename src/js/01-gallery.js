54c8ce719b4a1f8b96651d9b28867c9a98a17def
Julie Shapovalenko <jools.shapo@gmail.com>
2023-02-15 12:15:46 +0200

    add task-01

---
+++ src/js/01-gallery.js
@@ -0,0 +1,43 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery')
+
+const galleryMarkup = galleryItems.map((item) =>
+    `<div class="gallery__item">
+  <a class="gallery__link" href=${item.original} >
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original} 
+      alt=${item.description}
+      width="340"
+    />
+  </a>
+</div>`).join('')
+
+
+    
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup)
+
+galleryEl.addEventListener('click', onImageClick)
+
+function onImageClick(event) {
+    event.preventDefault();
+        
+    if (!event.target.classList.contains('gallery__image')) {
+        return
+    }
+
+    const imageEl = event.target.dataset.source;  
+    
+    const instance = basicLightbox.create(`
+    <img src=${imageEl} width="1280" >
+`)   
+    
+  instance.show()
+
+}
+
