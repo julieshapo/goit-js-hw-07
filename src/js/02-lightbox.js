05f02bd8ab67b7817a8d23f3204afc6258a36bdd
Julie Shapovalenko <jools.shapo@gmail.com>
2023-02-15 12:17:34 +0200

    add task-02

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -3,3 +3,21 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const galleryEl = document.querySelector('.gallery')
+
+const galleryMarkup = galleryItems.map((item) =>
+`<li>
+    <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+</li>`).join('')
+    
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup)
+
+const lightboxScroll = new SimpleLightbox('.gallery a', { 
+    nav: true,
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+ });
